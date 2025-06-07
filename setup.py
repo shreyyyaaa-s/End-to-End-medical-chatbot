a42aedb584adb0f603e5206c4a8a1eb38d052779
shreya <shreyabalamkar15@gmail.com>
2025-06-07 13:39:47 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import  find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    versions= '0.0.0',
+    author='Shreya S',
+    author_email='shreyabalamkar15@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    )
